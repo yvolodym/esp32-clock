692297c84355a2490b31410cf3ff04ff155edb1f
Eugen Vladimirski <eugen@softindustry.de>
2024-11-27 18:54:20 +0100

    init

--- main/esp32-clock.c
+++ main/esp32-clock.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "spi_flash_mmap.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include <inttypes.h>
+
 
 void app_main(void)
 {
+    printf("Hello world!\n");
 
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    uint32_t size_flash_chip;
+
+    esp_chip_info(&chip_info);
+    printf("This is ESP8266 chip with %d CPU cores, WiFi, ",
+            chip_info.cores);
+
+    printf("silicon revision %d, ", chip_info.revision);
+
+    esp_flash_get_size(NULL, &size_flash_chip);
+
+    printf("%"PRIu32"MB %s flash\n", size_flash_chip,
+            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    for (int i = 10; i >= 0; i--) {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
 }
