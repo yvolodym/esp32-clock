3c8724f812f3e1849298d19da7ea9b669acf99b9
Eugen Vladimirski <eugen@softindustry.de>
2025-05-30 22:23:32 +0200

    init

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -1,10 +1,10 @@
 #include <Arduino.h>
-#include "WiFiManager.h"
 #include "time.h"
 #include <SPI.h>
 #include <TFT_eSPI.h>     // https://github.com/Bodmer/TFT_eSPI
-#include "WifiTimeLib.h"
 #include "nvs_flash.h"
+#include "esp_log.h"
+#include "wifi.h"
 
 /*
 
@@ -75,6 +75,7 @@ uint16_t bg_colors[num_displays] = {TFT_DARKGREEN, TFT_BLUE};
 void wifi_init(void);
 static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data);
 static EventGroupHandle_t s_wifi_event_group;
+static int s_retry_num = 0;
 
 // Time 
 tm timeinfo;
@@ -306,12 +307,10 @@ void loop() {
 
     if (millis() % 3000 == 0){
         // report FPS every 3 seconds
-        Serial.print(" FPS > ");
-        Serial.println(fps);
+        ESP_LOGE(TAG, "FPS > %d", fps);
         avg_fps = (avg_fps + fps)/2;
         fps = 0;
     }
-
   }
 }
 
@@ -352,62 +351,6 @@ extern "C" void app_main() {
     
 }
 
-/* WiFi should start before using ESPNOW */
-void wifi_init(void)
-{
-    s_wifi_event_group = xEventGroupCreate();
-
-    ESP_ERROR_CHECK(esp_netif_init());
-
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    esp_netif_create_default_wifi_sta();
-    esp_netif_create_default_wifi_ap();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    esp_event_handler_instance_t instance_any_id;
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-                                                        ESP_EVENT_ANY_ID,
-                                                        &event_handler,
-                                                        NULL,
-                                                        &instance_any_id));
-    esp_event_handler_instance_t instance_got_ip;
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
-                                                        IP_EVENT_STA_GOT_IP,
-                                                        &event_handler,
-                                                        NULL,
-                                                        &instance_got_ip));
-
-    ESP_ERROR_CHECK(esp_wifi_start() );
-
-    ESP_LOGI(TAG, "wifi_init finished.");
-
-    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
-     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
-/*    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-        WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-        pdFALSE,
-        pdFALSE,
-        portMAX_DELAY);*/
-
-    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
-     * happened. */
-    /*if (bits & WIFI_CONNECTED_BIT) {
-        ESP_LOGI(TAG, "Successfully connected to AP");
-    } else if (bits & WIFI_FAIL_BIT) {
-        ESP_LOGI(TAG, "Failed to connect to AP");
-    } else {
-        ESP_LOGE(TAG, "UNEXPECTED EVENT");
-    }*/
-
-}
-
-static void event_handler(void* arg, esp_event_base_t event_base,
-                                int32_t event_id, void* event_data)
-{
-}
   
 /*
 
